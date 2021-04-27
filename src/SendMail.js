a1986f729fff7edf687836ccf41f6e98c4a65c4f
Samvit20 <samvit999@gmail.com>
2021-04-27 08:46:51 +0530

    Gmail-Clone

---
+++ src/SendMail.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./SendMail.css";
+import CloseIcon from "@material-ui/icons/Close";
+import { Button } from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { closeSendMessage } from "./features/mailSlice";
+
+function SendMail() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const dispatch = useDispatch();
+
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
+
+  return (
+    <div className="sendMail">
+      <div className="sendMail__header">
+        <h3>New Message</h3>
+        <CloseIcon
+          onClick={() => dispatch(closeSendMessage())}
+          className="sendMail__close"
+        />
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name='to'
+          placeholder="To"
+          type="email"
+          ref={'to', register({ required: true })}
+        />
+        {errors.to && <p className="sendMail__error">To is required !!</p>}
+
+        <input
+          name='subject'
+          placeholder="Subject"
+          type="text"
+          ref={'subject', register({ required: true })}
+        />
+
+        {errors.subject && <p className="sendMail__error">Subject is required !!</p>}
+        <input
+          name='message'
+          placeholder="Message"
+          type="text"
+          className="sendMail__message"
+          ref={'message', register({ required: true })}
+        />
+        {errors.message && <p className="sendMail__error">Message is required !!</p>}
+        
+        <div className="sendMail__options">
+          <Button
+            className="sendMail__send"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Send
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SendMail;
